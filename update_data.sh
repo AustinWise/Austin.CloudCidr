5402c9e70311841eba32d980bc648f22c15e4a68
Austin Wise <AustinWise@gmail.com>
2023-06-29 23:41:16 -0700

    Update cloud CIDR ranges

--- update_data.sh
+++ update_data.sh
@@ -6,4 +6,5 @@ cd -P -- "$(dirname -- "$0")"
 
 curl https://www.gstatic.com/ipranges/cloud.json -o data/google-cloud.json
 curl https://ip-ranges.amazonaws.com/ip-ranges.json -o data/aws.json
-curl https://download.microsoft.com/download/7/1/D/71D86715-5596-4529-9B13-DA13A5DE5B63/ServiceTags_Public_20230410.json -o data/azure.json
+# Seriously?
+curl -sf 'https://www.microsoft.com/en-us/download/confirmation.aspx?id=56519' | grep -Eo 'href="https[^"]+ServiceTags_Public_[^"]+\.json\"' | head -n 1 | sed -e 's/href="//' -e 's/"$//' | xargs curl -o data/azure.json
